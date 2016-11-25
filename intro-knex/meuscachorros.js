bc0482176757e80dc88bccdcb5979be4178106f3
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-25 18:21:54 -0300

    Correção de comentários :P

--- intro-knex/meuscachorros.js
+++ intro-knex/meuscachorros.js
@@ -2,18 +2,18 @@
 "use strict"
 const knex = require("./db");
 
-// queremos um nome para este convidado
+// queremos um nome e raça para este cachorro
 if(!process.argv[3]){
   console.log("usage: node meuscachorros.js <nome> <raca>");
   process.exit(0);
 }
 
-// inserir convidados
+// inserir cachorro
 knex("cachorroExercicio").insert({
 
   nomecachorro:process.argv[2],
   marcacachorro:process.argv[3]
-  
+
 },"idcachorro").then((ret) => {
   console.log(`Cachorro inserido com a chave ${ret[0]}`);
   process.exit(0);
